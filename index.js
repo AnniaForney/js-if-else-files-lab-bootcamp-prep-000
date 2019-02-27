c895a35f9385b5fb54b8e4664b128fd27a637c0a
Annia Forney <anniaforney@gmail.com>
2019-02-27 20:23:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 var name = "Joe"
-if (varname === "Joe") {
-            console.log("The name is correct")
-}
-
 var height = "74"
-if (var height === "74") {
-            console.log("The name is correct")
-}
- if (index.height === 74)  
-            console.log("The height is correct")
+
 // Don't worry about this
 module.exports = { name, height
 }
